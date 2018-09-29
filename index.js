47e433be688e19b109eb43c6fbf79aa86f377082
Jason Madar <jason@vandrico.com>
2018-09-29 11:55:42 -0700

    all the basic elements to make it a full stack app.  front end could use some work

--- index.js
+++ index.js
@@ -1,12 +1,52 @@
 
-sqlite = require('sqlite');
+let express = require('express');
+let app = express();
+
+let sqlite = require('sqlite');
+
+
+function setupServer(db) {
+
+
+    app.use(express.static('public'));
+        
+    app.get('/info', (req, res) => {
+        res.send('Full stack example');
+    });
+
+    // retrieve all unique stree names
+    app.get('/streets', (req, res) => {
+        db.all(`SELECT DISTINCT(name) FROM BikeRackData`)
+          .then( data => {
+              console.log(data);
+              res.send(data);
+          });
+    });
+
+    app.get('/streets/:street/', (req, res) => {
+        let streetName = req.params.street;
+        // query based on street
+        db.all(`SELECT * FROM BikeRackData WHERE name = '${streetName}'`)
+          .then( data => {
+              res.send(data);              
+          });
+        
+
+    });
+
+    
+
+    let server = app.listen(8080, () => {
+        console.log('Server ready', server.address().port);
+    });
+    
+}
 
 sqlite.open('database.sqlite').then( db => {
 	//console.log('database opened', db);
-	
-	return db.all('SELECT * from TEST');
-}).then( res => {
-	//console.log(res);
-	
-	console.log(JSON.stringify(res));
-});
+
+    setupServer(db);
+    //return db.all('SELECT * from TEST');
+    
+})
+
