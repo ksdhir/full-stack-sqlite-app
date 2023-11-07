5eb94615066888c6be35b6052abf46600a981e16
Karan Singh Dhir <ksinghdhir1@gmail>
2023-11-07 09:38:31 +0000

    add middleware to authenticate jwt

--- index.js
+++ index.js
@@ -3,18 +3,47 @@ const sqlite3 = require('sqlite3');
 const sqlite = require('sqlite');
 const express = require('express');
 const app = express();
+ const validateJwt = require('./validateJwt');
+// import validateJwt  from './validateJwt'
+
+
+
+
+async function middleware(req, res, next) {
+	const authorizationHeader = req.headers.authorization;
+	if(!authorizationHeader) {
+		res.status(401).json({messsage: 'Unauthorized: Missing Token'})
+	}
+
+	const tokenParts = authorizationHeader.split(" ");
+	if(tokenParts.length !== 2 || tokenParts[0] !== "Bearer") {
+		res.status(401).json({message: "Unauthorized: Invalid Token Format"})
+	}
+	const token = tokenParts[1];
+	const validation = await validateJwt(token);
+	console.log(validation, 'validation')
+	if(validation) {
+		next();
+	} else {
+		res.status(401).json({message: "Unauthorized- Invalid Token"})
+	}
+}
+
+
+
+
 
 function setupServer(db) {
 
     // This is a test frontend - uncomment to check it out
     // app.use(express.static('public'));
     
-    app.get('/info', (req, res) => {
+    app.get('/info', middleware, (req, res) => {
         res.send('Full stack example');
     });
 
     // retrieve all unique stree names
-    app.get('/streets', (req, res) => {
+    app.get('/streets', middleware, (req, res) => {
         db.all(`SELECT DISTINCT(name) FROM BikeRackData`)
           .then( data => {
               console.log(data);
@@ -22,7 +51,7 @@ function setupServer(db) {
           });
     });
 
-    app.get('/streets/:street/', (req, res) => {
+    app.get('/streets/:street/', middleware, (req, res) => {
         let streetName = req.params.street;
         // query based on street
 	// NOTE: this is open to SQL injection attack
